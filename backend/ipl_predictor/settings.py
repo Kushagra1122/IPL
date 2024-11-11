feb035658c7858d683266b7a799e5798bd4cdc65
Kushagra1122 <kushagratiwari.231ee128@nitk.edu.in>
2024-11-12 02:13:33 +0530

    Initial commit

--- backend/ipl_predictor/settings.py
+++ backend/ipl_predictor/settings.py
@@ -60,6 +60,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = "ipl_predictor.urls"
+CORS_ALLOW_METHODS = ["GET", "POST", "OPTIONS"]
+CORS_ALLOW_HEADERS = ["Content-Type"]
 
 TEMPLATES = [
     {
